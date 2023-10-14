58e5567ccc5724504bbef1dcb81a2b4aa3fe42f5
wefixte <cassy.pernelle@gmail.com>
2023-10-14 13:20:47 +0200

    modification for betty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/** Write a C program that prints the size of various types
+/**
+ * main - Write a C program that prints the size of various types
  * on the computer it is compiled and run on.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
