36e2d3dde26924442675e566bd5bd03c48d59932
wefixte <cassy.pernelle@gmail.com>
2023-10-14 12:30:45 +0200

    modification for betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main -  prints exactly \"Programming is like building a multilingual puzzle,
+ * followed by a new line
+ *
+ * Return: Always 0
+*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
