fbe8fb4b8c2a3fb89fb2581f523276a588f7afbe
wefixte <cassy.pernelle@gmail.com>
2023-10-17 16:18:21 +0200

    correction for betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,15 +4,19 @@
 
 /**
  * _islower - check for lowercase character
+ * @c : character to verified
  *
  * Return: 1 or 0
  */
 
 int _islower(int c)
 {
-	
 	if (c >= 'a' && c <= 'z')
-	return (1);
-
-	return (0);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
