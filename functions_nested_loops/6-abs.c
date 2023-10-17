9b694d64580293347efbeaa62a89e83f09ed43d0
wefixte <cassy.pernelle@gmail.com>
2023-10-17 17:23:46 +0200

    task 6 add num in function and prototype

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int num)
+{
+	if (num > 0)
+	{
+		return (num);
+	}
+	else
+	{
+		return (-num);
+	}
+}
