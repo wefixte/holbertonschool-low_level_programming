5a28f955abe7f07f42fc081dabefbc189525d8ab
wefixte <cassy.pernelle@gmail.com>
2023-10-18 14:49:54 +0200

    modification finale

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,34 +13,34 @@ void times_table(void)
 	x = 0;
 	while (x <= 9)
 	{
-			y = 1;
-			_putchar('0');
-			_putchar(',');;
-			while (y <= 9)
+		y = 1;
+		_putchar('0');
+		_putchar(',');
+		while (y <= 9)
+		{
+			z = x * y;
+			if (z <= 9)
 			{
-				z = x * y;
-				if (z <= 9)
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+				if (y < 9)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0');
-					if (y < 9)
-					{
 					_putchar(',');
-					}
 				}
-				else
-				{
-					_putchar(' ');
-					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
-					if (y < 9)
-					{
-					_putchar(',');
-					}
-				}
-				y++;
 			}
+			else
+			{
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+				if (y < 9)
+				{
+					_putchar(',');
+				}
+			}
+			y++;
+		}
 		x++;
 		_putchar('\n');
 	}
