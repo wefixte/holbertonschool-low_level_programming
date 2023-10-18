8b5421b50d8b3a7269da09eeb190ad2be28006b8
wefixte <cassy.pernelle@gmail.com>
2023-10-18 15:14:23 +0200

    function add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,10 +2,13 @@
 
 /**
  * add - fuction that adds two integers and returns the result
+ * @n1: first number to add
+ * @n2: second number to add
  *
  * Return: two integers
  */
 
-int add(int, int)
+int add(int n1, int n2)
 {
+	return (n1 + n2);
 }
