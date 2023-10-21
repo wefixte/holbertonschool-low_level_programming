e2119abf37462916ea520265e6a46f892fb1e87e
wefixte <cassy.pernelle@gmail.com>
2023-10-21 18:26:56 +0200

    add main and function 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function prints numbers from 0 to 9
+ * but skip 2 and 4
+ *
+ * Return: 0.
+ */
+
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (!(a == '2' || a == '4'))
+			_putchar(a);
+	}
+	_putchar('\n');
+}
