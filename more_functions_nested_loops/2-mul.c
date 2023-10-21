96715201baa16bbed75a3ce5baa0e54b26d73318
wefixte <cassy.pernelle@gmail.com>
2023-10-21 17:37:32 +0200

    add function multiplies & prototype

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - function that multiplies two integers
+* @a: first int
+* @b: second int
+*
+* Return: result of  multiplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
