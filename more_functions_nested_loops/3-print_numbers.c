5c48460d072411e226e3d70f0508f37fa94377f0
wefixte <cassy.pernelle@gmail.com>
2023-10-21 18:10:09 +0200

    modif main.h and 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - function that prints numbers from 0 to 9
+*
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
