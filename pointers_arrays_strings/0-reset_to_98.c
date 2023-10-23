b55f2bc6b3528146c880a52b71326fe0e113df83
wefixte <cassy.pernelle@gmail.com>
2023-10-23 14:08:14 +0200

    add task 0 function and main.h

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 : function that tales a pointer to an int as parameter
+ * @n: number int
+ *
+ * Return : void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
