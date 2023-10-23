ba53f98f3fd3e7e4a904853b64508b52c08353a1
wefixte <cassy.pernelle@gmail.com>
2023-10-23 17:11:59 +0200

    function _puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
