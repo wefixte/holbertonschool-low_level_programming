bdd5056a44302cc0e2720d0e429ec2fadf086687
wefixte <cassy.pernelle@gmail.com>
2023-10-24 15:13:00 +0200

    test

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - function that returns the length of a string
+ * @s: string of characters
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0' ; length++)
+		;
+	return (length);
+}
+
+/**
  * _strcpy - function that copies the string
  * @dest: pointeur
  * @src: pointeur
@@ -12,10 +28,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	do {
+	for (; i <= _strlen(src); i++)
 		dest[i] = src[i];
-		i++;
-	} while(src[i] != '\0');
 
 	return (dest);
 }
