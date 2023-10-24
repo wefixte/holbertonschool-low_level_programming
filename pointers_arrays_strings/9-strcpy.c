d2edcd76b6ad54311accae81e421862d3947a8a5
wefixte <cassy.pernelle@gmail.com>
2023-10-24 14:04:07 +0200

    function copies

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string
+ * @dest: pointeur
+ * @src: pointeur
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
