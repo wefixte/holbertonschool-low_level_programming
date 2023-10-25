84164fb3472b526d8b71e537db1b4452b64306d2
wefixte <cassy.pernelle@gmail.com>
2023-10-25 09:28:32 +0200

    betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _atoi - function that convert a string to an integer 
-* @s: string
-* 
-* Return: int
+*_atoi - function that convert a string to an integer
+*@s: string
+*
+*Return: int
 */
 
 int _atoi(char *s)
