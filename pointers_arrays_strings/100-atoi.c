defbf5d5de5d05e3356d3fdc64f6a7df37f3776e
wefixte <cassy.pernelle@gmail.com>
2023-10-25 09:25:50 +0200

    pour les points!

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _atoi - function that convert a string to an integer 
+* @s: string
+* 
+* Return: int
+*/
+
+int _atoi(char *s)
+{
+	int i;
+
+	i = *s++;
+	return (i);
+}
