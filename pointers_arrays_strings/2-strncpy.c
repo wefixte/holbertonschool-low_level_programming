451dd996a8119e07872c93746ef8f0a0e871cbb2
wefixte <cassy.pernelle@gmail.com>
2023-10-25 12:27:26 +0200

    add function cpy and proto

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**_strncpy - function that copies a string
+*@dest: destination
+*@src: source
+*@n: nombre
+*
+*Return: char
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
