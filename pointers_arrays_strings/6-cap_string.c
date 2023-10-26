4b653d109eae00fda97068e34f68162193704fef
wefixte <cassy.pernelle@gmail.com>
2023-10-26 21:29:52 +0200

    function that capitalizes all word -> add if for first letter

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,10 @@ char *cap_string(char *ptr)
 {
 	int i;
 
-	for (i = 0; ptr[i] != '\0'; i++)
+	if (ptr[0] >= 'a' && ptr[0] <= 'z')
+		ptr[0] = ptr[0] - 'a' + 'A';
+
+	for (i = 1; ptr[i] != '\0'; i++)
 	{
 		if ((ptr[i] >= 'a' && ptr[i] <= 'z') &&
 		(ptr[i - 1] == ' ' || ptr[i - 1] == '\t' || ptr[i - 1] == '\n' ||
