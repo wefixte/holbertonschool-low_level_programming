5bcf0baac3b5a6ac4fb21e83f26c04ddafec6c22
wefixte <cassy.pernelle@gmail.com>
2023-10-28 17:01:48 +0200

    modif function locates while loop

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,20 +3,22 @@
 
 /**
 *_strchr - function that locates a character in a string
-*@s: string
+*@s: string pointer
 *@c: character
 *Return: a pointer to the first occurence of c or NULL
 */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
-		if (*s == c)
-		{
+		if (*s != c)
+			s++;
+		else
 			return (s);
-		}
-		s++;
 	}
+	if (c == '\0')
+		return (s);
+
 	return (NULL);
 }
