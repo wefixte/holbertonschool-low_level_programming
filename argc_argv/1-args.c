be60f45ec2751facf00c9a6d0c072fb645a75e0c
wefixte <cassy.pernelle@gmail.com>
2023-11-02 18:37:36 +0100

    program prints number arguments passed

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - program prints number of arguments passed into it
+*@argv: value
+*@argc: number
+
+*Return: 0
+*/
+
+int main(int argc, __attribute__((unused))  char **argv)
+{
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
