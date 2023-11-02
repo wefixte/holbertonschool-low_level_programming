650959404583aa290ca7f8f6c0ecf1717a81d42a
wefixte <cassy.pernelle@gmail.com>
2023-11-02 18:45:10 +0100

    bettttyyyyyyyy

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,8 +4,8 @@
 *main - program prints number of arguments passed into it
 *@argv: value
 *@argc: number
-
-*Return: 0
+*
+*Return: 0.
 */
 
 int main(int argc, __attribute__((unused))  char **argv)
