a8bc50e1631e1fc5aa06c52a3427018912d142ff
wefixte <cassy.pernelle@gmail.com>
2023-11-03 15:41:49 +0100

    task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - program that multiplies two numbers
+*
+*Return: 1
+*/
+
+int main(int argc, char **argv)
+{
+	int i = atoi(argv[1]);
+	int y = atoi(argv[2]);
+	int result = i * y;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", result);
+	return (0);
+}
