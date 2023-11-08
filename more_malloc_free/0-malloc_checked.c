aaac5b2e35c377421d26d695a16f65cb658db5ca
wefixte <cassy.pernelle@gmail.com>
2023-11-08 09:48:14 +0100

    Projet 2 and task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: size memory to allocate
+ * Return: a pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+	/**
+	 * pointer mem, any type of data, malloc to dynamically allocate
+	 * b bytes of memory and assigns the adress of the bloc to the pointer
+	*/
+	if (mem == NULL)
+		exit (98);
+	
+	return (mem);
+}
