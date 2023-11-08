518b19052eba923f7c041e6f2a3f10d4abea10de
wefixte <cassy.pernelle@gmail.com>
2023-11-08 10:56:26 +0100

    betty

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -11,13 +11,15 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+	void *mem
+
+	mem = malloc(b);
 	/**
 	 * pointer mem, any type of data, malloc to dynamically allocate
 	 * b bytes of memory and assigns the adress of the bloc to the pointer
 	*/
 	if (mem == NULL)
-		exit (98);
-	
+		exit(98);
+
 	return (mem);
 }
