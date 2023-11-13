91c10e3eaf6df29c58bdf0b39228102ca1f49e51
wefixte <cassy.pernelle@gmail.com>
2023-11-13 10:49:35 +0100

    Task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure pour les chiens
+ * @name: nom
+ * @age: age
+ * @owner: propriétaire
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
