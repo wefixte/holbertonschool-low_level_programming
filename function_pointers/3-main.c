0178f6d0e2eb0ba5dcae47ea1091a6ece47927f8
wefixte <cassy.pernelle@gmail.com>
2023-11-15 19:35:13 +0100

    task3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+
+/**
+ * main - main function
+ * @argc: compte argument
+ * @argv: arguments
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/*if number of argument wrong*/
+
+	num1 = atoi(argv[1]);
+	operator = get_op_func(argv[2]);
+	num2 = atoi(argv[3]);
+	/*convert num1 & 2 into intergers and function pointer for 2nd place arg*/
+
+	if (operator == NULL || (argv[2][1] != '\0'))
+	/*check the lenght of the operator string with argv*/
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/*if operator not add, sub, mul, div ou modulo*/
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/*if user tries to divide by 0*/
+
+	printf("%d\n", operator(num1, num2));
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of a and b
+ * @a: int
+ * @b: int
+ * Return: result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of a and b
+ * @a: int
+ * @b: int
+ * Return: result
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ * @a: int
+ * @b: int
+ * Return: result
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of a and b
+ * @a: int
+ * @b: int
+ * Return: result
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division of a and b
+ * @a: int
+ * @b: int
+ * Return: result
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
