2d2fa71402aa4828fe58f72ae1ab0bf30dd47e2c
wefixte <cassy.pernelle@gmail.com>
2023-11-15 13:35:46 +0100

    betty fixed

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  * giver as a parameter on each element of an array
  * @array: array
  * @size: size of the array
- * @action: function pointer 
+ * @action: function pointer
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
