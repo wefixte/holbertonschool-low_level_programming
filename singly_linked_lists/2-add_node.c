d52577e152d0eb8c2d3606ea30477fcb9c045a5c
wefixte <cassy.pernelle@gmail.com>
2023-11-29 12:31:27 +0100

    Create function add node beginning list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list
+ * @headerfile:
+ * @str:
+ * Return:
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t * new_node;
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
