86f591a21b82976233b1f336297123336369b175
wefixte <cassy.pernelle@gmail.com>
2023-12-01 13:48:30 +0100

    create function return value of bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: number
+ * @index: index
+ * Return:the value of the bit at index index or -1 if an error occured
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int size = sizeof(n) * 8;
+
+	if (index > size)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
