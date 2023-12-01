16706031f029283d2268f851c7797569573c4f21
wefixte <cassy.pernelle@gmail.com>
2023-12-01 10:12:33 +0100

    Add librairies and _putchar prototype

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*Librairies*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /*Prototypes*/
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
