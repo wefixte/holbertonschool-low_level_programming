6d50c9dc6723b1498273495dd6ad7d360808918d
wefixte <cassy.pernelle@gmail.com>
2023-12-06 15:10:21 +0100

    correct missing condition and exit 100

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
 {
 	int file_source, file_destination, read_file, write_file;
-	char buffer[1024]; /*blocs of 1024 bytes*/
+	char buffer[1024]; /*blocks of 1024 bytes*/
 
 	if (argc != 3)
 	{
@@ -24,11 +24,14 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	file_destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_destination < 0)
 	{
 		dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
 		exit(99);
 	}
+
 	while ((read_file = read(file_source, buffer, sizeof(buffer))) > 0)
 	{
 		write_file = write(file_destination, buffer, read_file);
@@ -38,14 +41,11 @@ int main(int argc, char **argv)
 			exit(99);
 		}
 	}
-	if (close(file_source) < 0)
+
+	if (close(file_source) < 0 || close(file_destination) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_source);
-		exit(100);
-	}
-	if (close(file_destination) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_destination);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			(close(file_source) < 0) ? file_source : file_destination);
 		exit(100);
 	}
 	return (0);
